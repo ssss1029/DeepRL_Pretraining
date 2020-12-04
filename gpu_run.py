5a68e3860bd554414a70466d20029ac533aa4d2e
Saurav Kadavath <sauravkadavath@berkeley.edu>
2020-12-04 08:53:40 +0800

    running no_pretraining_PixelEncoderFullGroupConvBigger on 3 environments

--- gpu_run.py
+++ gpu_run.py
@@ -18,7 +18,7 @@ class Config:
     # Shared args to put onto all of the JOBS
     SHARED_ARGS = ""
 
-    HEADER = "conda activate rl-pad-3; "
+    HEADER = "conda activate pad; "
 
     # Specifies tasks to run. It maps tmux session name to the command to run in that session.
     JOBS = {
@@ -43,8 +43,8 @@ class Config:
                 --save_model",
         
         "cheetah_run_noss_no_pretraining_PixelEncoderFullGroupConvBigger_seed0" : "python3 src/train.py \
-                --domain_name walker \
-                --task_name walk \
+                --domain_name cheetah \
+                --task_name run \
                 --action_repeat 4 \
                 --mode train \
                 --num_shared_layers 8 \
